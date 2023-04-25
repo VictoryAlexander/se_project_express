870af01e4e463b76450545a598b9b112a4bc567d
Victor Alexander <victoryalexander@outlook.com>
2023-04-24 21:06:39 -0700

    Fixed errorhandler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,8 @@
 const errorHandler = (err, req, res, next) => {
-  res.status(err.statusCode).send({ message: err.message });
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'An error occurred on the server' : message,
+  });
 }
 
 module.exports = errorHandler;
