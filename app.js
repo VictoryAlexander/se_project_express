cfadb77e3a9e0e556a08b07bda4d5c0d66d8cb3f
Victor Alexander <victoryalexander@outlook.com>
2023-04-28 17:04:44 -0700

    Added crash test for review

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ app.use(cors());
 app.use(limiter);
 app.use(express.json());
 app.use(requestLogger);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
 app.use(routes);
 app.use(errorLogger);
 app.use(errors());
